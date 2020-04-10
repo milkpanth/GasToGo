352a1aef5bfa2424e8ce55ce81543306c33680b5
milkpanth <milkpanth@yahoo.com>
2020-04-10 23:41:50 +0700

    databasefail

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,16 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
 
     GMSServices.provideAPIKey("AIzaSyDuDt1q8xpeK145Ngf5Y34INGt3XNHnZ_Q")
     
